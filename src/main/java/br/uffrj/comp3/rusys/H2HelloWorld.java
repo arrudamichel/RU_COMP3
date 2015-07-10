51a62d5062866b6ee2fa8785fc70f0dfce2757f3
Michel Arruda <micheldarruda@yahoo.com.br>
2015-07-10 18:21:05 -0300

    BD mudado

--- src/main/java/br/uffrj/comp3/rusys/H2HelloWorld.java
+++ src/main/java/br/uffrj/comp3/rusys/H2HelloWorld.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.servlet.annotation.WebServlet;
+
 import org.h2.tools.DeleteDbFiles;
 
 public class H2HelloWorld {
@@ -16,20 +18,20 @@ public class H2HelloWorld {
      */
     public static void main(String... args) throws Exception {
         // delete the database named 'test' in the user home directory
-        DeleteDbFiles.execute("~", "test", true);
+        //DeleteDbFiles.execute("~", "test", true);
 
         Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/git/RU_COMP3/RU","sa","sa");
         Statement stat = conn.createStatement();
 
         // this line would initialize the database
         // from the SQL script file 'init.sql'
         // stat.execute("runscript from 'init.sql'");
 
-        stat.execute("create table test(id int primary key, name varchar(255))");
-        stat.execute("insert into test values(1, 'Hello')");
+        //stat.execute("create table TEST(id int primary key, name varchar(255))");
+        stat.execute("insert into TEST values(3, 'Bugo3')");
         ResultSet rs;
-        rs = stat.executeQuery("select * from test");
+        rs = stat.executeQuery("select * from TEST");
         while (rs.next()) {
             System.out.println(rs.getString("name"));
         }
