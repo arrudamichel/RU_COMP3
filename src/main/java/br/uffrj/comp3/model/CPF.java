4f3b537ea33c392395a9764f9b928435391867b2
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-07-12 20:44:23 -0300

    validacao momentaneamente removida

--- src/main/java/br/uffrj/comp3/model/CPF.java
+++ src/main/java/br/uffrj/comp3/model/CPF.java
@@ -5,31 +5,35 @@ public class CPF {
 
 	public CPF(String cpf) {
 		super();
-		if (valida(cpf))
+		//if (valida(cpf))
 			this.cpf = cpf;
-		else
-			throw new UnsupportedOperationException();
+		//else
+		//	throw new UnsupportedOperationException();
 	}
 
 	private boolean valida(String s) {
+		if (s.length() != 11)
+			return false;
+		
+		
 		int[] a = new int[9];
 		char[] cpf = s.toCharArray();
-        for (int i = 0; i < cpf.length ; i++) {
-            if (Character.isDigit(cpf[i]))
-                a[i] = cpf[i] - '0';
-        }
-     
-        int b1 = 0, b2 = 0;
+		for (int i = 0; i < cpf.length; i++) {
+			if (Character.isDigit(cpf[i]))
+				a[i] = cpf[i] - '0';
+		}
 
-        for (int i = 1; i < 10; i++) {
-            b1 += i * a[i - 1];
-		    b2 += (10 - i) * a[i - 1];
-        }
+		int b1 = 0, b2 = 0;
 
-        b1 = getDig(b1);
-        b2 = getDig(b2);
-     
-        return (b1 == a[9] && b2 == a[10]); 
+		for (int i = 1; i < 10; i++) {
+			b1 += i * a[i - 1];
+			b2 += (10 - i) * a[i - 1];
+		}
+
+		b1 = getDig(b1);
+		b2 = getDig(b2);
+
+		return (b1 == a[9] && b2 == a[10]);
 	}
 
 	private int getDig(int b) {
