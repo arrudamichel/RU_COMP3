b7979f8783e35691add8c0ad1eba69820f2be51b
Liliane <lilianeneves8@gmai.com>
2015-07-14 14:21:57 -0300

    Classes de Teste

---
+++ src/test/java/br/uffrj/comp3/rusys/model/AlunoTest.java
@@ -0,0 +1,31 @@
+package br.uffrj.comp3.rusys.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlunoTest {
+
+	@Test
+	public void testAlunoStringIntStringSexoEnumTituloEnumStringCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAluno() {
+		fail("Not yet implemented");
+		
+	}
+	@Test
+	public void testGetCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCurso() {
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/br/uffrj/comp3/rusys/model/CursoTest.java
@@ -0,0 +1,63 @@
+package br.uffrj.comp3.rusys.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CursoTest {
+
+	@Test
+	public void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCursoStringStringDepartamento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNome() {
+		
+		String nome = "Lili";
+		
+		assertEquals("Lili", nome);
+		
+	}
+
+	@Test
+	public void testGetSigla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSigla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepartamento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDepartamento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIdentificador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIdentificador() {
+		fail("Not yet implemented");
+	}
+
+}
