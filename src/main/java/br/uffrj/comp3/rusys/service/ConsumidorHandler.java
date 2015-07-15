1c7615563fb77866d38201411c30c3f86cc4bdef
odrand <bla@blals.com>
2015-07-15 11:49:55 -0300

    Coisas de tiket

--- src/main/java/br/uffrj/comp3/rusys/service/ConsumidorHandler.java
+++ src/main/java/br/uffrj/comp3/rusys/service/ConsumidorHandler.java
@@ -2,12 +2,23 @@ package br.uffrj.comp3.rusys.service;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import br.uffrj.comp3.rusys.model.Aluno;
 import br.uffrj.comp3.rusys.model.Consumidor;
+import br.uffrj.comp3.rusys.model.Curso;
+import br.uffrj.comp3.rusys.model.Departamento;
+import br.uffrj.comp3.rusys.model.Funcionario;
+import br.uffrj.comp3.rusys.model.SexoEnum;
+import br.uffrj.comp3.rusys.model.TituloEnum;
 import br.uffrj.comp3.rusys.model.vo.ConsumidorVO;
+import br.uffrj.comp3.rusys.persintece.AlunoGateway;
 import br.uffrj.comp3.rusys.persintece.ConnectionFactory;
 import br.uffrj.comp3.rusys.persintece.ConsumidorGateway;
+import br.uffrj.comp3.rusys.persintece.CursoGateway;
+import br.uffrj.comp3.rusys.persintece.DepartamentoGateway;
 import br.uffrj.comp3.rusys.util.Constantes;
 
 public class ConsumidorHandler
@@ -28,6 +39,26 @@ public class ConsumidorHandler
 		conn.close();
 		
 		return rs.getFetchSize();
+	}
+	
+	public static Consumidor recuperarConsumidor(int idConsumidor) throws SQLException, Exception
+	{
+		Consumidor consumidor = null;
+		
+		Aluno aluno = AlunoHandler.recuperarAluno(idConsumidor);
+		
+		Funcionario funcionario = FuncionarioHandler.recuperarFuncionario(idConsumidor);
+
+		if (aluno!=null) 
+		{
+			consumidor = aluno;
+		} 
+		else if (funcionario!=null) 
+		{
+			consumidor = funcionario;
+		}
+		
+		return consumidor;
 	}
 
 	public static void excluirConsumidor(Consumidor consumidor) throws Exception
--- src/main/java/br/uffrj/comp3/rusys/service/TicketHandler.java
+++ src/main/java/br/uffrj/comp3/rusys/service/TicketHandler.java
@@ -4,114 +4,125 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
-import br.uffrj.comp3.rusys.model.Aluno;
 import br.uffrj.comp3.rusys.model.Consumidor;
-import br.uffrj.comp3.rusys.model.Curso;
-import br.uffrj.comp3.rusys.model.Departamento;
-import br.uffrj.comp3.rusys.model.Funcionario;
 import br.uffrj.comp3.rusys.model.Refeicao;
-import br.uffrj.comp3.rusys.model.SexoEnum;
 import br.uffrj.comp3.rusys.model.Ticket;
 import br.uffrj.comp3.rusys.model.vo.TicketVO;
-import br.uffrj.comp3.rusys.persintece.AlunoGateway;
 import br.uffrj.comp3.rusys.persintece.ConnectionFactory;
 import br.uffrj.comp3.rusys.persintece.ConsumidorGateway;
-import br.uffrj.comp3.rusys.persintece.CursoGateway;
-import br.uffrj.comp3.rusys.persintece.DepartamentoGateway;
-import br.uffrj.comp3.rusys.persintece.FuncionarioGateway;
 import br.uffrj.comp3.rusys.persintece.RefeicaoGateway;
 import br.uffrj.comp3.rusys.persintece.TicketGateway;
 import br.uffrj.comp3.rusys.util.Constantes;
 
 public class TicketHandler {
 
-	public static void cadastrarTicket(TicketVO ticketVO) throws Exception {
-//		Connection conn = ConnectionFactory.getConnection(Constantes.DBPATH, Constantes.USER, Constantes.PASS);
-//
-//		TicketGateway ticketGateway = new TicketGateway(conn);
-//
-//		ConsumidorGateway consumidorGateway = new ConsumidorGateway(conn);
-//
-//		RefeicaoGateway refeicaoGateway = new RefeicaoGateway(conn);
-//
-//		ResultSet rs = consumidorGateway.selecionarConsumidorPorMatricula(ticketVO.getMatricula());
-//
-//		int consumId = -1;
-//
-//		rs.next();
-//		consumId = rs.getInt(1);
-//
-//		rs = refeicaoGateway.selecionarRefeicaoPorId(ticketVO.getRefeicao());
-//
-//		int refeicaoId = -1;
-//
-//		rs.next();
-//		refeicaoId = rs.getInt(1);
-//
-//		ArrayList<Object> valores = new ArrayList<Object>(
-//				Arrays.asList(consumId, refeicaoId, ticketVO.getValor(), ticketVO.isPago()));
-//
-//		if (!ticketGateway.inserir(valores))
-//			throw new Exception("falha.ao.cadastrar.ticket");
-//
-//		conn.close();
+	public static void cadastrarTicket(TicketVO ticketVO) throws Exception 
+	{
+		Connection conn = ConnectionFactory.getConnection(Constantes.DBPATH, Constantes.USER, Constantes.PASS);
+
+		TicketGateway ticketGateway = new TicketGateway(conn);
+		ConsumidorGateway consumidorGateway = new ConsumidorGateway(conn);
+		RefeicaoGateway refeicaoGateway = new RefeicaoGateway(conn);
+
+		ResultSet rs = consumidorGateway.selecionarConsumidorPorMatricula(ticketVO.getMatricula());
+
+		int consumId = -1;
+
+		rs.next();
+		consumId = rs.getInt(1);
+
+		rs = refeicaoGateway.selecionarRefeicaoPorId(ticketVO.getRefeicao());
+
+		int refeicaoId = -1;
+
+		rs.next();
+		refeicaoId = rs.getInt(1);
+
+		ArrayList<Object> valores = new ArrayList<Object>(
+				Arrays.asList(consumId, refeicaoId, ticketVO.getValor(), ticketVO.isPago()));
+
+		if (ticketGateway.inserir(valores)==null)
+			throw new Exception("falha.ao.cadastrar.ticket");
+
+		conn.close();
 
 	}
+	
+	public static Collection<Ticket> recuperarTickets(TicketVO ticketVO) throws Exception
+	{
+//		Aluno aluno = new Aluno(id, nome, matricula, anoDeIngresso, curso);
+		
+		ArrayList<Ticket> tickets = new ArrayList<>();
+		Connection conn = ConnectionFactory.getConnection(Constantes.DBPATH, Constantes.USER, Constantes.PASS);
 
-	public static Ticket recuperarTicket(int id) throws Exception {
-		/*
-		 * CREATE TABLE IF NOT EXISTS "ticket" ( "ticket_id" INT NOT NULL
-		 * AUTO_INCREMENT, "consumidor_matricula" INT NOT NULL,
-		 * "refeicao_idRefeicao" INT NOT NULL, "preco" DECIMAL(10,2) NOT NULL,
-		 * "pago" TINYINT(1) NOT NULL,
-		 */
-//
-//
-//		Connection conn = ConnectionFactory.getConnection(Constantes.DBPATH, Constantes.USER, Constantes.PASS);
-//		TicketGateway ticketGateway = new TicketGateway(conn);
-//
-//		ResultSet rs = ticketGateway.selecionarTicketPorId(id);
-//
-//		rs.next();
-//		int consuId = rs.getInt(2);
-//
-//		ConsumidorGateway consuGateway = new ConsumidorGateway(conn);
-//
-//		AlunoGateway alunoGateway = new AlunoGateway(conn);
-//		FuncionarioGateway funcGateway = new FuncionarioGateway(conn);
-//
-//		ResultSet funcSet = funcGateway.selecionarFuncionarioPorMatricula(consuId);
-//		ResultSet aluSet = alunoGateway.selecionarAlunoPorMatricula(consuId);
-//		ResultSet consuSet = consuGateway.selecionarConsumidorPorMatricula(consuId);
-//		
-//		// ("matricula","nome","ano_ingresso","sexo","titulo","cpf","situacao")
-//		Consumidor consumidor;
-//		if (funcSet.isBeforeFirst()) {
-//			consumidor = new Funcionario();		
-//		} else if (aluSet.isBeforeFirst()) {
-//			consumidor = new Aluno();
-//		} else {
-//			throw new Exception("consumidor.nao.econtrado");
-//		}
-//		
-//		consumidor.setMatricula(consuId);
-//		consumidor.setNome(consuSet.getString(2));
-//		consumidor.setAnoDeIngresso(consuSet.getString(3));
-//		consumidor.setSexo(consuSet.getString(4).equals("M") ? SexoEnum.M : SexoEnum.F);
-//		consumidor.setCpf(consuSet.getString(6));
-//
-//		//public Ticket(int id, boolean pago,Consumidor consumidor, Refeicao refeicao)
-//		// ("consumidor_matricula", "refeicao_idRefeicao", "preco", "pago")
-//		Ticket ticket = new Ticket(id);
-//		ticket.setConsumidor(consumidor);
-//		ticket.setValor(rs.getFloat(3));
-//		ticket.setPago((rs.getInt(4) == 1) ? true : false);
-//
-//
-//		conn.close();
-//
-//		return ticket;
-		return null;
+		TicketGateway ag = new TicketGateway(conn);
+		ResultSet rsTickect = ag.selecionarTickets();
+
+		Ticket ticket = null;
+		
+		while (rsTickect.next())
+		{
+		
+			int consuId = rsTickect.getInt(2);
+			int refeicaoId = rsTickect.getInt(2);
+			
+			// ("matricula","nome","ano_ingresso","sexo","titulo","cpf","situacao")
+			Consumidor consumidor = ConsumidorHandler.recuperarConsumidor(consuId);
+			
+			Refeicao refeicao = RefeicaoHandler.recuperarRefeicao(refeicaoId);
+
+			//public Ticket(int id, boolean pago,Consumidor consumidor, Refeicao refeicao)
+			// ("consumidor_matricula", "refeicao_idRefeicao", "preco", "pago")
+			
+			boolean pago = rsTickect.getInt(4) == 1 ? true : false;
+			
+			ticket = new Ticket(consuId,pago , consumidor, refeicao);
+			
+			tickets.add(ticket);
+		}
+		
+		conn.close();
+		
+		return tickets;
+	}
+
+	public static Ticket recuperarTicket(int id) throws Exception 
+	{
+		Connection conn = ConnectionFactory.getConnection(Constantes.DBPATH, Constantes.USER, Constantes.PASS);
+		
+		TicketGateway ticketGateway = new TicketGateway(conn);
+		ResultSet rs = ticketGateway.selecionarTicketPorId(id);
+		rs.next();
+		int consuId = rs.getInt(2);
+		int refeicaoId = rs.getInt(2);
+		
+		// ("matricula","nome","ano_ingresso","sexo","titulo","cpf","situacao")
+		Consumidor consumidor = ConsumidorHandler.recuperarConsumidor(consuId);
+		
+		Refeicao refeicao = RefeicaoHandler.recuperarRefeicao(refeicaoId);
+
+		//public Ticket(int id, boolean pago,Consumidor consumidor, Refeicao refeicao)
+		// ("consumidor_matricula", "refeicao_idRefeicao", "preco", "pago")
+		
+		boolean pago = rs.getInt(4) == 1 ? true : false;
+		
+		Ticket ticket = new Ticket(consuId,pago , consumidor, refeicao);
+		
+		conn.close();
+
+		return ticket;
+	}
+	
+	public static void excluirTicket(Ticket ticket) throws Exception
+	{	
+		Connection conn = ConnectionFactory.getConnection(Constantes.DBPATH, Constantes.USER, Constantes.PASS);
+		TicketGateway cg = new TicketGateway(conn);
+
+		if (!cg.excluirTicket(ticket.getId()))
+			throw new Exception("falha.ao.excluir.Ticket");
+		
+		conn.close();
 	}
 }
