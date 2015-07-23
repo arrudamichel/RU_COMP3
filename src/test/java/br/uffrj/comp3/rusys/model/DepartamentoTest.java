ac1a7b4376958822daa5511afac1bf9dd2c720f0
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-07-23 19:09:41 -0300

    DepartamentoTest funcionando

--- src/test/java/br/uffrj/comp3/rusys/model/DepartamentoTest.java
+++ src/test/java/br/uffrj/comp3/rusys/model/DepartamentoTest.java
@@ -6,103 +6,33 @@ import org.junit.Test;
 
 public class DepartamentoTest {
 
-	Departamento  de;
+	private Departamento dept;
 	
+
 	@Test
-	public void testHashCode() throws Exception {
-		int esperado = 2043512536;
-		de = new Departamento(1,"Depto", "DE");
-		
-		assertEquals(esperado,de.hashCode());
+	public void testDepartamentoOk() throws Exception {
+		dept = new Departamento(1,"Depto", "DE");
+	}
+
+	@Test(expected = Exception.class)
+	public void testDepartamentoSiglaNull() throws Exception {
+		dept = new Departamento(1,"Depto", null);
 	}
 
 	@Test
-	public void testDepartamento() throws Exception {
-		de = new Departamento(1,"Depto", "DE");
-			
-	}
-
-	@Test
-	public void testDepartamentoStringString() throws Exception {
-		String esperado = "Depto";
-		de = new Departamento(1,"Depto", "DE");
-
-		assertEquals(esperado,de.getNome());
-	}
-
-	@Test
-	public void testGetNome() throws Exception {
-		String esperado = "Depto";
-		de = new Departamento(1,"Depto", "DE");	
-
-		assertEquals(esperado,de.getNome());
-	}
-
-	@Test
-	public void testSetNome() throws Exception {
-		String esperado = "Depto 2";
-		de = new Departamento(1,"Depto", "DE");
-		de.setNome("Depto 2");
-		
-
-		assertEquals(esperado,de.getNome());
-	}
-
-	@Test
-	public void testGetSigla() throws Exception {
-		String esperado = "DE";
-		de = new Departamento(1,"Depto", "DE");
-		
-
-		assertEquals(esperado,de.getSigla());
-	}
-
-	@Test
-	public void testSetSigla() throws Exception {
+	public void testSetSiglaOk() throws Exception {
 		String esperado = "DER";
-		de = new Departamento(1,"Depto", "DE");
-		de.setSigla("DER");
+		dept = new Departamento(1,"Depto", "DE");
+		dept.setSigla("DER");
 		
 
-		assertEquals(esperado,de.getSigla());
+		assertEquals(esperado,dept.getSigla());
 	}
-
-	@Test
-	public void testGetIdentificador() throws Exception {
-		int esperado = 1;
-		de = new Departamento(1,"Depto", "DE");
-		
-		
-		assertEquals(esperado,de.getIdentificador());
-		
+	
+	@Test(expected = Exception.class)
+	public void testSetSiglaNull() throws Exception {
+		String esperado = "DER";
+		dept = new Departamento(1,"Depto", "DE");
+		dept.setSigla(null);
 	}
-
-	@Test
-	public void testSetIdentificador() throws Exception {
-		int esperado = 2;
-		de = new Departamento(1,"Depto", "DE");
-		de.setIdentificador(2);
-		
-		assertEquals(esperado,de.getIdentificador());
-	}
-
-	@Test
-	public void testEqualsObject() throws Exception {
-		boolean esperado = true;
-		de = new Departamento(1,"Depto", "DE");
-		
-		assertEquals(esperado,de.equals(de));
-	}
-
-	@Test
-	public void testGetId() throws Exception {
-		int esperado = 1;
-		de = new Departamento(1,"Depto", "DE");
-		
-		
-		assertEquals(esperado,de.getId());
-		
-	}
-
-
 }
