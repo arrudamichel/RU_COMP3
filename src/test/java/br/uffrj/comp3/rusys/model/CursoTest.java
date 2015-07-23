d99033341437cff729b42f6c444a94b9f88b7bed
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-07-23 18:54:38 -0300

    CursoTest funcionando

--- src/test/java/br/uffrj/comp3/rusys/model/CursoTest.java
+++ src/test/java/br/uffrj/comp3/rusys/model/CursoTest.java
@@ -5,108 +5,57 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CursoTest {
-	
-	Departamento de;
-	Curso cu;
-	
-	
+
+	private Departamento dept;
+	private Curso curso;
 
 	@Test
-	public void testCurso() throws Exception {
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
+	public void testCursoOk() throws Exception {
+		dept = new Departamento(1, "Depto", "DCC");
+		curso = new Curso(1, "computacao", "CC", dept);
 	}
 
-	@Test
-	public void testCursoStringStringDepartamento() throws Exception {
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-		
-		assertEquals("computacao",cu.getNome());	
-		
+	@Test(expected = Exception.class)
+	public void testCursoSemSigla() throws Exception {
+		dept = new Departamento(1, "Depto", "DCC");
+		curso = new Curso(1, "computacao", null, dept);
 	}
 
-	@Test
-	public void testGetNome() throws Exception {
-		String esperado = "computacao";
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-	
-		assertEquals(esperado, cu.getNome());
+	@Test(expected = Exception.class)
+	public void testCursoSemDepartamento() throws Exception {
+		curso = new Curso(1, "computacao", "CC", null);
 	}
 
-	@Test
-	public void testSetNome() throws Exception {
-		String esperado = "Letras";
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-		cu.setNome("Letras");
-		
-	
-		assertEquals(esperado, cu.getNome());
-		
-	}
+	@Test(expected = Exception.class)
+	public void testSetSiglaNull() throws Exception {
+		dept = new Departamento(1, "Depto", "DCC");
+		curso = new Curso(1, "computacao", "CC", dept);
+		curso.setSigla(null);
 
-	@Test
-	public void testGetSigla() throws Exception {
-		
-		 String esperado = "CC";
-		 de = new Departamento(1, "Depto", "DCC");
-			cu = new Curso(1, "computacao", "CC", de);
-		
-		
-	
-		assertEquals(esperado, cu.getSigla());
 	}
 
 	@Test
 	public void testSetSigla() throws Exception {
-		 String esperado = "CComp";
-		 de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-		 cu.setSigla("CComp");
-		
+		String esperado = "CComp";
+		dept = new Departamento(1, "Depto", "DCC");
+		curso = new Curso(1, "computacao", "CC", dept);
+		curso.setSigla("CComp");
+
+		assertEquals(esperado, curso.getSigla());
+	}
 	
-		assertEquals(esperado, cu.getSigla());
+	@Test(expected = Exception.class)
+	public void testSetDepartamentoNull() throws Exception {
+		dept = new Departamento(1, "Depto", "DCC");
+		curso = new Curso(1, "computacao", "CC", dept);
+		curso.setDepartamento(null);
 	}
 
 	@Test
-	public void testGetDepartamento() throws Exception {
-		String esperado = "DCC";
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-			
-		assertEquals(esperado, cu.getDepartamento().getSigla());
+	public void testSetDepartamentoOk() throws Exception {
+		Departamento de2 = new Departamento(2, "Depto de Computacao", "DCC1");
+		dept = new Departamento(1, "Depto", "DCC");
+		curso = new Curso(1, "computacao", "CC", dept);
+		curso.setDepartamento(de2);
 	}
-
-	@Test
-	public void testSetDepartamento() throws Exception {
-		String esperado = "DCC1";
-		Departamento de2 = new Departamento(2,"Depto de Computacao","DCC1");
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-		cu.setDepartamento(de2);
-			
-		assertEquals(esperado, cu.getDepartamento().getSigla());
-	}
-
-	@Test
-	public void testGetIdentificador() throws Exception {
-		int esperado = 1;
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-			
-		assertEquals(esperado, cu.getIdentificador());
-		
-	}
-
-	@Test
-	public void testSetIdentificador() throws Exception {
-		int esperado = 1;
-		de = new Departamento(1, "Depto", "DCC");
-		cu = new Curso(1, "computacao", "CC", de);
-			
-		assertEquals(esperado, cu.getIdentificador());
-	}
-
 }
