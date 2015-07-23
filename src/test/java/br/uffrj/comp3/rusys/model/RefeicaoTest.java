7218f807cdf196e01aac85e3618b1e9f0459f0b3
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-07-23 20:35:59 -0300

    RefeicaoTest funcionando

--- src/test/java/br/uffrj/comp3/rusys/model/RefeicaoTest.java
+++ src/test/java/br/uffrj/comp3/rusys/model/RefeicaoTest.java
@@ -5,80 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RefeicaoTest {
-	
-	Refeicao re;
-	
+
+	Refeicao ref;
 
 	@Test
-	public void testRefeicao() throws Exception {
-		re= new Refeicao(1,"arroz"){};
-		
+	public void testRefeicaoOk() throws Exception {
+		ref = new Refeicao(1, "Refeicao", "opVeg", TurnoEnum.MANHA);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testRefeicaoFail() throws Exception {
+		ref = new Refeicao(1, null, "opVeg", TurnoEnum.MANHA);
 	}
 
 	@Test
-	public void testGetDescricao() throws Exception {
+	public void testSetDescricaoOk() throws Exception {
 		String esperado = "arroz";
-		re= new Refeicao(1,"arroz"){};
-		
-		assertEquals(esperado,re.getDescricao());
-	}
+		ref = new Refeicao(1, "Refeicao", "opVeg", TurnoEnum.MANHA);
+		ref.setDescricao("arroz");
 
-	@Test
-	public void testSetDescricao() throws Exception {
-		String esperado = "arroz";
-		re= new Refeicao(1,"feijao"){};
-		re.setDescricao("arroz");
-		
-		assertEquals(esperado,re.getDescricao());
-	}
-
-	@Test
-	public void testGetOpcaoVeg() throws Exception {
-		String esperado = "arroz";
-		re= new Refeicao(1,"feijao"){};
-		re.setOpcaoVeg("arroz");
-		
-		assertEquals(esperado,re.getOpcaoVeg());
-	}
-
-	@Test
-	public void testSetOpcaoVeg() throws Exception {
-		String esperado = "arroz";
-		re= new Refeicao(1,"feijao"){};
-		re.setOpcaoVeg("arroz");
-		
-		assertEquals(esperado,re.getOpcaoVeg());
+		assertEquals(esperado, ref.getDescricao());
 	}
 	
-
-	@Test
-	public void testGetTurno() throws Exception {
-		
-		re= new Refeicao(1,"arroz"){};
-		re.setTurno(TurnoEnum.MANHA);
-		
-		assertEquals(TurnoEnum.MANHA,re.getTurno());
+	@Test(expected = Exception.class)
+	public void testSetDescricaoFail() throws Exception {
+		ref = new Refeicao(1, null, "opVeg", TurnoEnum.MANHA);
+		ref.setDescricao(null);
 	}
-	
-
-	@Test
-	public void testSetTurno() throws Exception {
-		re= new Refeicao(1,"arroz"){};
-		re.setTurno(TurnoEnum.MANHA);
-		
-		assertEquals(TurnoEnum.MANHA,re.getTurno());
-	}
-
-	
-
-	@Test
-	public void testGetIdentificador() throws Exception {
-		re= new Refeicao(1,"arroz"){};
-		
-		
-		
-		
-		assertEquals(1,re.getIdentificador());
-	}
-
 }
